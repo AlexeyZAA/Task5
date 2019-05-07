cae8c43cb88a7cbf05723044ec6f054a3726079f
zaa <zaa@tk-barn.ru>
2019-05-07 08:35:03 +0700

    Hello world gеределан на мультистраницы

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
-const Koa = require('koa');
-const app = new Koa();
+'use strict';
+const messages = require('./controllers/messages');
+const compress = require('koa-compress');
+const logger = require('koa-logger');
+const serve = require('koa-static');
+const route = require('koa-route');
+const koa = require('koa');
+const path = require('path');
+const app = module.exports = koa();
 
-// response
-app.use(ctx => {
-  ctx.body = 'Hello World';
-});
+// Logger
+app.use(logger());
 
-app.listen(3000);
+app.use(route.get('/', messages.home));
+app.use(route.get('/messages', messages.list));
+app.use(route.get('/messages/:id', messages.fetch));
+app.use(route.post('/messages', messages.create));
+app.use(route.get('/async', messages.delay));
+app.use(route.get('/promise', messages.promise));
+
+// Serve static files
+app.use(serve(path.join(__dirname, 'public')));
+
+// Compress
+app.use(compress());
+
+if (!module.parent) {
+  app.listen(3000);
+  console.log('listening on port 3000');
+}
